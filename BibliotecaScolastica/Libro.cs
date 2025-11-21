dd418e25ac0a2bcb9bbec4421b89fd2808a5500a
Federico Minotti <FEDERICO.MINOTTI@local.ittcesena.it>
2025-11-21 08:33:25 +0100

    R1.0.0:Prima implementazione codice in Libro.cs

--- BibliotecaScolastica/Libro.cs
+++ BibliotecaScolastica/Libro.cs
@@ -8,5 +8,22 @@ namespace BibliotecaScolastica
 {
     internal class Libro
     {
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int AnnoPubblicazione { get; set; }
+        public string Editore { get; set; } 
+        public int NumeroPagine { get; set; }
+
+        public int redingTime(int paginePerOra)
+        {
+            if (paginePerOra <= 0)
+            {
+                throw new ArgumentException("Il numero di pagine per ora deve essere maggiore di zero.");
+            }
+            return NumeroPagine / paginePerOra;
+        }
+
+
+
     }
 }
